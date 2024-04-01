7b087049a0f44e0f7b0925d839ab83decb6bbc10
Mahdi Salahinia <salahimahdi938@gmail.com>
2024-04-01 13:24:43 +0330

    AddExtentionForGetMentionWithRegex

---
+++ Twitter.Clone.Tweets/Extensions/TextScanner.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Twitter.Clone.Tweets.Extensions;
+
+public class TextScanner
+{
+    public static List<string> GetMentions(string content)
+    {
+        var mentions = new List<string>();
+
+        var regex = new Regex(@"(?<=@)\w+");
+
+        var matches = regex.Matches(content);
+
+        foreach (Match item in matches)
+        {
+            mentions.Add(item.Value);
+        }
+
+        return mentions;
+    }
+}
