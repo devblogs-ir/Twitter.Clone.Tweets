7d3cedd97822480a7259b747ca090c0ed3e267a7
Mahdi Salahinia <salahimahdi938@gmail.com>
2024-04-06 23:04:04 +0330

    ReverseChanges

--- Twitter.Clone.Tweets/BackgroundServices/Channel/ReaderBackgroundService.cs
+++ Twitter.Clone.Tweets/BackgroundServices/Channel/ReaderBackgroundService.cs
@@ -4,28 +4,40 @@ using Twitter.Clone.Tweets.Models.Contracts;
 
 namespace Twitter.Clone.Tweets.BackgroundServices.Channel;
 
-public class ReaderBackgroundService(Channel<CreateTweetContext> channel, TextScanner textScanner) : BackgroundService
+public class ReaderBackgroundService(Channel<CreateTweetContext> channel, TextScanner textScanner, ILogger<ReaderBackgroundService> logger) : BackgroundService
 {
     private readonly Channel<CreateTweetContext> _channel = channel;
     private readonly TextScanner _textScanner = textScanner;
+    private readonly ILogger<ReaderBackgroundService> _logger = logger;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        _logger.LogInformation("ReaderBackgroundService is starting.");
+
+        try
         {
-            if (await _channel.Reader.WaitToReadAsync(stoppingToken))
+            await foreach (var item in _channel.Reader.ReadAllAsync(stoppingToken))
             {
-                while (_channel.Reader.TryRead(out var item))
-                {
-                    var mentions = _textScanner.GetMentions(item.Content);
+                _logger.LogInformation("Processing item with {Content}: ", item.Content);
 
-                    var hashtags = _textScanner.GetHashtags(item.Content);
+                var mentions = _textScanner.GetMentions(item.Content);
+                var hashtags = _textScanner.GetHashtags(item.Content);
 
-                    //Do something with mentions and hashtags
-                }
+                //RabbitMq
+
+                _logger.LogInformation("processed successfully.");
             }
-
-            await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
         }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while processing items.");
+            throw;
+        }
+    }
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("ReaderBackgroundService is stopping.");
+        await base.StopAsync(cancellationToken);
     }
 }
